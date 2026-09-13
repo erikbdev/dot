bf8083ecb8dc056114ee9ec0fd812f002e7dc050
Erik Bautista Santibanez <me@erikb.dev>
2026-09-13 02:39:38 -0700

    wip

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,13 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Toggle comment with Ctrl+/, on top of the default gcc/gc.
--- Most terminals (including ones without a proper key protocol) send <C-/>
--- as <C-_>, so both are mapped to be safe.
-local function map_toggle_comment(lhs)
-  vim.keymap.set("n", lhs, "gcc", { remap = true, desc = "Toggle comment line" })
-  vim.keymap.set("v", lhs, "gc", { remap = true, desc = "Toggle comment" })
-end
-
-map_toggle_comment("<C-/>")
-map_toggle_comment("<C-_>")
+-- Toggle comment, on top of the default gcc/gc.
+vim.keymap.set("n", "<leader>/", "gcc", { remap = true, desc = "Toggle comment line" })
+vim.keymap.set("v", "<leader>/", "gc", { remap = true, desc = "Toggle comment" })
